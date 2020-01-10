893fb49aae23606afc6629ad00bcca3bdd8f13c2
K3be <ahmed.13.alkaaby@gmail.com>
2020-01-10 17:36:30 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -10,8 +10,9 @@ class Timer extends Component {
     };
   }
 
-  //Your code here
-
+this.timer.current.style.color=
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
